9b8e3d8b710a724afe872b3dc16f732650e44490
Valentine Shlyagin <vshlyagin@inbox.ru>
2022-02-28 20:39:54 +0300

    lab2 commit

---
+++ lab2/lab2.rb
@@ -0,0 +1,23 @@
+puts
+
+def greeting(name, secondName, old)
+		if (old < 18)
+				puts "Привет, #{name} #{secondName}. Тебе меньше 18-ти лет, но начать учиться программировать никогда не рано"
+		else 
+			puts "Привет, #{name} #{secondName}. Самое время занятся делом"
+		end
+end
+
+greeting "Илья", "Бондарчук", 17
+
+
+
+def foobar(number1, number2)
+		if (number1 == 20 || number2 == 20)
+			return number2
+		else 
+			puts (number2 + number1)
+		end
+end
+
+puts foobar 23, 22
