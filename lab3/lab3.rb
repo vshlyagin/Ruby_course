1fee3c0978b70c41727f2658e2b4da4f766b16a9
Valentine Shlyagin <vshlyagin@inbox.ru>
2022-03-09 20:49:31 +0300

    lab3

---
+++ lab3/lab3.rb
@@ -0,0 +1,42 @@
+puts 
+
+def closeOnCS(word)
+	if (word[-2..-1] == "CS")
+		puts 2**word.size
+	else 
+		puts word.reverse
+	end
+end
+
+print "Enter a word : "
+
+words = gets.to_s.chomp
+
+closeOnCS words
+
+puts
+
+puts
+
+def spisokPokemonov()
+    print "Enter number of Pokemons: "
+    n = gets.to_i
+    puts
+    pokemons = Hash.new{n}
+    i = 0
+    while i < n
+        i += 1
+        print "Enter a pokemon name: " 
+        pockemon_name = gets.to_s.chomp
+        print "Enter its color: "
+        pockemon_color = gets.to_s.chomp
+        puts
+        pokemons[pockemon_name] = pockemon_color
+    end
+    
+    puts "Pokemon list you created: "
+    pokemons.each { |x, y| puts "Name:#{x}: Color:#{y}, " }
+end
+
+spisokPokemonov
+
