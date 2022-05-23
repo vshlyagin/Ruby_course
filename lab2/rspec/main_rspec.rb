cf57819a4ad69eb6f4f41016c0129ee20e6f6a8a
Valentine <vshlyagin@inbox.ru>
2022-05-24 00:52:34 +0300

    commit

--- lab2/rspec/main_rspec.rb
+++ lab2/rspec/main_rspec.rb
@@ -3,15 +3,15 @@ require_relative 'main'
 
 RSpec.describe "greetings" do
   it "do ok for <18" do
-    str = "Hello, Max Kovalskiy. You are younger then 18, but you can start programming just now!"
+    str = "Привет, Валентин Шлягин. Тебе меньше 18-ти лет, но начать учиться программировать никогда не рано"
     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].each do |value|
-      RSpec.expect(greetings("Max", "Kovalskiy", value)).to eq str
+      RSpec.expect(greetings("Валентин", "Шлягин", value)).to eq str
     end
   end
-  it  "do wrong for >18" do
-    str = "Hello, Max Kovalskiy. You are younger then 18, but you can start programming just now!"
-    [19, 33, 44, 55].each do |value|
-      RSpec.expect(greetings("Max", "Kovalskiy", value)).to eq str
+  it  "do ok for >18" do
+    str = "Привет, Валентин Шлягин. Самое время занятся делом"
+    [19, 22, 34, 65].each do |value|
+      RSpec.expect(greetings("Валентин", "Шлягин", value)).to eq str
     end
   end
 end
