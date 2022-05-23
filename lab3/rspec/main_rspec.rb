cc8a41f61bcc37b1ac8c41e22cd0f346cbbdedf6
Valentine <vshlyagin@inbox.ru>
2022-05-24 01:04:05 +0300

    commit

--- lab3/rspec/main_rspec.rb
+++ lab3/rspec/main_rspec.rb
@@ -4,10 +4,10 @@ require_relative 'main'
 RSpec.describe "closeOnCS" do
   it "do ok for close on CS" do
     str = "wefwspfspfCS"
-    RSpec.expect(closeOnCS(str)).to eq 2 ** str.length
+      RSpec.expect(closeOnCS(str)).to eq 2 ** str.length
   end
   it  "do ok for no close on CS" do
     str = "aspfapsfpas"
-    RSpec.expect(closeOnCS(str)).to eq str.reverse
+      RSpec.expect(closeOnCS(str)).to eq str.reverse
   end
 end
